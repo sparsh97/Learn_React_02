95716d2080615dc53ede4e7c8bc5288821e5a30d
sparsh97 <sparshvermadmc@gmail.com>
2024-01-28 21:12:32 +0530

    axios added and api call added for creating books

--- books/src/App.jsx
+++ books/src/App.jsx
@@ -1,20 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import BookList from "./components/BookList";
 import BookCreate from "./components/BookCreate";
+import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
-  const createBook = (title) => {
-    setBooks([...books, { id: Math.round(Math.random() * 1000), title }]);
-    console.log(title);
+  const fetchAllBooks = async () => {
+    const response = await axios.get("http://127.0.0.1:3001/books");
+    setBooks(response.data);
   };
+
+  useEffect(() => {
+    fetchAllBooks();
+  }, [books]);
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://127.0.0.1:3001/books", {
+      title,
+    });
+    setBooks([...books, response?.data]);
+  };
+
   const deleteBookbyId = (id) => {
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
+
   const updateBookTitle = (id, title) => {
     const updateBooks = books.map((book) => {
       if (book.id == id) {
@@ -24,6 +38,7 @@ function App() {
     });
     setBooks(updateBooks);
   };
+  
   return (
     <>
       <div className="p-2 m-2 flex flex-col justify-center items-center">
