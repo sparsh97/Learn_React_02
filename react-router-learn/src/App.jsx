28a95254e96d8869afc889c54cb8a0245cfd090e
sparsh97 <sparshvermadmc@gmail.com>
2024-02-06 23:10:03 +0530

    condition based css

--- react-router-learn/src/App.jsx
+++ react-router-learn/src/App.jsx
@@ -2,19 +2,20 @@ import { useState } from "react";
 import "./App.css";
 import Button from "./components/Button";
 
+
 function App() {
   return (
     <>
-      <div className="p-1 m-1">
-        <Button primary secondary>Click Here</Button>
+      <div>
+        <Button primary>Click Here</Button>
       </div>
-      <div className="p-1 m-1">
+      <div>
         <Button secondary rounded> By now </Button>
       </div>
-      <div className="p-1 m-1">
+      <div>
         <Button warn outline> Download</Button>
       </div>
-      <div className="p-1 m-1">
+      <div>
         <Button danger rounded outline> OMG </Button>
       </div>
     </>
--- react-router-learn/src/components/Button.jsx
+++ react-router-learn/src/components/Button.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import className from "classnames";
+import { twMerge } from "tailwind-merge";
 
 function Button({
   children,
@@ -11,10 +13,19 @@ function Button({
   rounded,
   outline,
 }) {
+  const finalClassName = twMerge(
+    className("px-3", "py-1.5 border", {
+      "bg-blue-500 border-blue-500 text-white": primary,
+      "bg-gray-500 border-gray-500 text-white": secondary,
+      "bg-orange-500 border-orange-500 text-white": warn,
+      "bg-red-500 border-red-500 text-white": danger,
+      "bg-green-500 border-green-500 text-white": success,
+    })
+  );
   return (
     <>
-      <div className="p-2 m-2">
-        <button>{children}</button>
+      <div>
+        <button className={finalClassName}>{children}</button>
       </div>
     </>
   );
