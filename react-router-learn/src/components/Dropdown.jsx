476dd0820fb7ed5b5cafc1af28d6791beaee3e69
sparsh97 <sparshvermadmc@gmail.com>
2024-02-24 13:30:19 +0530

    condition added

--- react-router-learn/src/components/Dropdown.jsx
+++ react-router-learn/src/components/Dropdown.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Dropdown({options}) {
+    const [isOpen, setIsOpen] = useState(false);
     const renderOptions = options.map( op => {
         return <div key={op.value}>{op.label}</div>
-    })
+    });
+    const handleClick = () => {
+        setIsOpen(prev => !prev);
+    };
+
   return (
     <div>
-        <div>Select...</div>
-        <div>{renderOptions}</div>
+        <div onClick={handleClick}>Select...</div>
+        {isOpen && <div >{renderOptions}</div>}
     </div>
   )
 }
