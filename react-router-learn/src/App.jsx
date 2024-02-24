1a362cb33755f1cc0703893c047d8dc4153ba922
sparsh97 <sparshvermadmc@gmail.com>
2024-02-24 13:52:07 +0530

    label update on seclection and css added

--- react-router-learn/src/App.jsx
+++ react-router-learn/src/App.jsx
@@ -3,12 +3,24 @@ import "./App.css";
 import Dropdown from "./components/Dropdown";
 
 function App() {
+  const [select, setSelect] = useState(null);
+  const handleSelect = (option) => {
+    setSelect(option);
+  };
   const options = [
-    {value: 'red', label: 'Red'},
-    {value: 'blue', label: 'Blue'},
-    {value: 'green', label: 'Green'}
-  ]
-  return (<><Dropdown options={options}></Dropdown></>);
+    { value: "red", label: "Red" },
+    { value: "blue", label: "Blue" },
+    { value: "green", label: "Green" },
+  ];
+  return (
+    <>
+      <Dropdown
+        options={options}
+        selection={select}
+        onSelect={handleSelect}
+      ></Dropdown>
+    </>
+  );
 }
 
 export default App;
--- react-router-learn/src/components/Dropdown.jsx
+++ react-router-learn/src/components/Dropdown.jsx
@@ -1,20 +1,37 @@
-import React, { useState } from 'react'
-
-function Dropdown({options}) {
-    const [isOpen, setIsOpen] = useState(false);
-    const renderOptions = options.map( op => {
-        return <div key={op.value}>{op.label}</div>
-    });
-    const handleClick = () => {
-        setIsOpen(prev => !prev);
-    };
-
+import { useState } from "react";
+import PropTypes from "prop-types";
+function Dropdown({ options, selection, onSelect }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOptionClick = (value) => {
+    setIsOpen(false);
+    onSelect(value);
+  };
+  const renderOptions = options.map((op) => {
+    return (
+      <div className="p-1" onClick={() => handleOptionClick(op)} key={op.value}>
+        {op.label}
+      </div>
+    );
+  });
+  const handleClick = () => {
+    setIsOpen((prev) => !prev);
+  };
+  let content = 'Select.....';
+  if (selection) {
+    content = selection?.label;
+  }
   return (
-    <div>
-        <div onClick={handleClick}>Select...</div>
-        {isOpen && <div >{renderOptions}</div>}
+    <div className="p-2 m-1 borde rounded-2xl">
+      <div className="p-1 m-1 border border-black rounded-xl" onClick={handleClick}>{content}</div>
+      {isOpen && <div className="m-1">{renderOptions}</div>}
     </div>
-  )
+  );
 }
 
-export default Dropdown
+Dropdown.propTypes = {
+  options: PropTypes.array.isRequired,
+  selection: PropTypes.object,
+  onSelect: PropTypes.func.isRequired
+};
+
+export default Dropdown;
