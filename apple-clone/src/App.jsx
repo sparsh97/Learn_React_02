48dfae7c2f0f39755c0b4a4def301b4c36b5a24e
Sparsh Verma <sparshvermadmc@gmail.com>
2024-08-16 17:34:51 +0530

    highlights changes

--- apple-clone/src/App.jsx
+++ apple-clone/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Highlights from './components/Highlights'
+import Model from './components/Model'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   )
 }
