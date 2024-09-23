d1dd32913e79f029fe2ce2ebc340f63ef1056e97
Sparsh Verma <sparshvermadmc@gmail.com>
2024-09-23 21:11:31 +0530

    gym-react-webapp

---
+++ gym-react-webapp/src/components/Button.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function Button(props) {
+  const { text, func } = props;
+  return (
+    <button
+      onClick={func}
+      className="px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200"
+    >
+      <p>{text}</p>
+    </button>
+  );
+}
+
+export default Button;
